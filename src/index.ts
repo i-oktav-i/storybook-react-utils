f098e55789acdc10803170e720a0270d58b98ae5
Andrei Taranenko <andrei_taranenko@epam.com>
2022-02-03 23:21:30 +0300

    export fix

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ export type {
 
 export { getStoryCreator } from './getStoryCreator';
 export { containerWrapper, containerWrapperArgTypes } from 'wrappers/container';
-export { getCssVarWrapper } from 'wrappers/cssVar';
-export { getStorePropertyWrapper } from 'wrappers/storeProperty';
-export { getValueControlWrapper } from 'wrappers/valueControl';
-export { compose } from 'compose';
+export { getCssVarWrapper } from './wrappers/cssVar';
+export { getStorePropertyWrapper } from './wrappers/storeProperty';
+export { getValueControlWrapper } from './wrappers/valueControl';
+export { compose } from './compose';
